d0fdeb9beb3b9b8db89634368cd0302377009d5b
Steven Coulter <scoulter@assetgenie.com>
2015-06-23 10:36:35 -0400

    Turned off autoload for class_exists in Translator

--- lib/cricket/utils/Translator.php
+++ lib/cricket/utils/Translator.php
@@ -123,9 +123,9 @@ class Translator {
 			if(strpos($inPageClass, $thisPath) === 0) {
 				return $inPageClass;
 			}
-			if(class_exists($thisPath."\\".$inPageClass)) {
-                                return $thisPath."\\".$inPageClass;
-                        }
+			if(class_exists($thisPath."\\".$inPageClass, false)) {
+            	return $thisPath."\\".$inPageClass;
+            }
 		}
 		
 		foreach($inPageSearchPaths as $thisTestNS) {
